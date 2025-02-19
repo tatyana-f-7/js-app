8c58d067c2edd0e0015880139eacc8892359a436
tatyana-f-7 <tatyanaflores7@gmail.com>
2025-02-18 19:24:05 -0600

    added payment tests

--- test/search.js
+++ test/search.js
@@ -1,8 +1,7 @@
-function searchForProduct(str) {
-    if(str.trim().length === 0) console.log('Product name is not provided!');
-    console.log(`${str} is searched!`);
-}
-
-searchForProduct('Book');
-searchForProduct('');
-searchForProduct('  ');
+function searchForProduct(productName) {
+    if(productName.trim().length === 0) console.log('Product name is not provided');
+    else console.log(`${productName} is searched!`);
+  }
+  searchForProduct('Book');
+  searchForProduct('');
+  searchForProduct('    ');
