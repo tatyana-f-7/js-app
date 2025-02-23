7dcf6657b7cb39a4ea03d3d6d85ff46a9bf37618
tatyana-f-7 <tatyanaflores7@gmail.com>
2025-02-22 22:53:34 -0600

    new tests

--- test/search.js
+++ test/search.js
@@ -2,6 +2,7 @@ function searchForProduct(productName) {
     if(productName.trim().length === 0) console.log('Product name is not provided');
     else console.log(`${productName} is searched!`);
   }
-  searchForProduct('Book');
+  
+  console.log(searchForProduct('Book'));
   searchForProduct('');
   searchForProduct('    ');
